337c3368c91c1fac0ba42e70d057eb26a3880279
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 17:28:57 +0300

    Update اگاداری.py

--- AlinaXIQ/plugins/Yumi/اگاداری.py
+++ AlinaXIQ/plugins/Yumi/اگاداری.py
@@ -1,26 +1,26 @@
 from pyrogram import Client, filters
 from strings.filters import command
 from AlinaXIQ import app
-from AlinaXIQ.utils.Alina_ban import admin_filter
+from AlinaXIQ.utils.alina_ban import admin_filter
 
-7amo = {}
-Alina_IQ = 3
+ahmed = {}
+tom_max = 3
 
 @app.on_message(command("ئاگاداربە") & admin_filter)
-async def alinaa(client, message):
+async def tom(client, message):
     me = message.from_user.id
     user_id = message.reply_to_message.from_user.id
     chat_id = message.chat.id
-    if chat_id not in 7amo:
-        7amo[chat_id] = {}
-    if user_id not in 7amo[chat_id]:
-        7amo[chat_id][user_id] = 1
+    if chat_id not in ahmed:
+        ahmed[chat_id] = {}
+    if user_id not in ahmed[chat_id]:
+        ahmed[chat_id][user_id] = 1
     else:
-        7amo[chat_id][user_id] += 1
-    await message.reply_text(f"{7amo[chat_id][user_id]}")
-    if 7amo[chat_id][user_id] >= Alina_IQ:
+        ahmed[chat_id][user_id] += 1
+    await message.reply_text(f"{ahmed[chat_id][user_id]}")
+    if ahmed[chat_id][user_id] >= tom_max:
         try:
-        	del 7amo[chat_id][user_id]
+        	del ahmed[chat_id][user_id]
         	await client.ban_chat_member(chat_id, user_id)
         	await message.reply("**دەرکرا♥️✅•**")   	
         except:
