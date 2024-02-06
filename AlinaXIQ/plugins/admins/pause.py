2e3c9abe05a714bc12ec7cab854a5b484ec1d5b0
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 13:34:59 +0300

    Update pause.py

--- AlinaXIQ/plugins/admins/pause.py
+++ AlinaXIQ/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from strings.filters import command
-from DAXXMUSIC import app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import is_music_playing, music_off
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.utils.database import is_music_playing, music_off
+from AlinaXIQ.utils.decorators import AdminRightsCheck
+from AlinaXIQ.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await DAXX.pause_stream(chat_id)
+    await Alina.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
