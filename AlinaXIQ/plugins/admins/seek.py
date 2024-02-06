28cb34d43ec450f5bb4e3c072828a306bf00a262
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 13:37:23 +0300

    Update seek.py

--- AlinaXIQ/plugins/admins/seek.py
+++ AlinaXIQ/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import YouTube, app
-from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils import AdminRightsCheck, seconds_to_min
-from DAXXMUSIC.utils.inline import close_markup
+from AlinaXIQ import YouTube, app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.misc import db
+from AlinaXIQ.utils import AdminRightsCheck, seconds_to_min
+from AlinaXIQ.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await DAXX.seek_stream(
+        await Alina.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
