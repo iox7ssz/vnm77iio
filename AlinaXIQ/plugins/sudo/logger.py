52d6b528c9664bedd4596d368b5f602ff10eef5a
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 13:58:14 +0300

    Update logger.py

--- AlinaXIQ/plugins/sudo/logger.py
+++ AlinaXIQ/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import add_off, add_on
-from DAXXMUSIC.utils.decorators.language import language
+from AlinaXIQ import app
+from AlinaXIQ.misc import SUDOERS
+from AlinaXIQ.utils.database import add_off, add_on
+from AlinaXIQ.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
