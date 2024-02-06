c041213f3427c49636dfe83e342a02b5731c789e
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 13:02:34 +0300

    Update speedtest.py

--- AlinaXIQ/plugins/tools/speedtest.py
+++ AlinaXIQ/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.decorators.language import language
+from AlinaXIQ import app
+from AlinaXIQ.misc import SUDOERS
+from AlinaXIQ.utils.decorators.language import language
 
 
 def testspeed(m, _):
