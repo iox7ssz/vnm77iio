75abe8b28ccc10e30c19a6813fb890db2cbc56cc
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 12:20:55 +0300

    Update sys.py

--- AlinaXIQ/utils/sys.py
+++ AlinaXIQ/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from DAXXMUSIC.misc import _boot_
-from DAXXMUSIC.utils.formatters import get_readable_time
+from AlinaXIQ.misc import _boot_
+from AlinaXIQ.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
