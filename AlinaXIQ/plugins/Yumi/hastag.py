dddd7aca55dc7889d6e55af24067855c317ea49e
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 12:39:37 +0300

    Update hastag.py

--- AlinaXIQ/plugins/Yumi/hastag.py
+++ AlinaXIQ/plugins/Yumi/hastag.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as  BSP
-from DAXXMUSIC import app as DAXX
+from AlinaXIQ import app as Alina
 from pyrogram import filters
 url = "https://all-hashtag.com/library/contents/ajax_generator.php"
 
-@DAXX.on_message(filters.command("hastag"))
+@Alina.on_message(filters.command("hastag"))
 async def hastag(bot, message):
     global content
     try:
