c80c6ebba75e02405edeee718af03391f7fa4817
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 13:46:40 +0300

    Update tts.py

--- AlinaXIQ/plugins/misc/tts.py
+++ AlinaXIQ/plugins/misc/tts.py
@@ -6,6 +6,6 @@ from AlinaXIQ import app
 @app.on_message(filters.command('tts'))
 def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
-    tts = gTTS(text=text, lang='hi')
+    tts = gTTS(text=text, lang='ckb')
     tts.save('speech.mp3')
     client.send_audio(message.chat.id, 'speech.mp3')
