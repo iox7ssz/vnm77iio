dae2eb3aa45e96d10893ba7d6ad3d06e31c97fa2
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 17:37:08 +0300

    Update approve.py

--- AlinaXIQ/plugins/bot/approve.py
+++ AlinaXIQ/plugins/bot/approve.py
@@ -52,8 +52,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "DAXXMUSIC/assets/userinfo.png"
-font_path = "DAXXMUSIC/assets/hiroko.ttf"
+bg_path = "AlinaXIQ/assets/userinfo.png"
+font_path = "AlinaXIQ/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
