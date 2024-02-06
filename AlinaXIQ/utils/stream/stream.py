c430415e1c153a3c083c0c826063028fd5dcbe0e
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-06 17:58:20 +0300

    Update stream.py

--- AlinaXIQ/utils/stream/stream.py
+++ AlinaXIQ/utils/stream/stream.py
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await DAXX.force_stop_stream(chat_id)
+        await Alina.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await DAXX.join_call(
+                await Alina.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await DAXXBin(msg)
+            link = await AlinaBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await DAXX.join_call(
+            await Alina.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await DAXX.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Alina.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await DAXX.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Alina.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await DAXX.join_call(
+            await Alina.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await DAXX.join_call(
+            await Alina.join_call(
                 chat_id,
                 original_chat_id,
                 link,
