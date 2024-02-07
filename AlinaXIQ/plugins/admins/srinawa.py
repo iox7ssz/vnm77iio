603f48de182c129cd23b8a5562059da491016e2f
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-08 02:55:38 +0300

    Create srinawa.py

---
+++ AlinaXIQ/plugins/admins/srinawa.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from AlinaXIQ import app
+
+
+@app.on_message(filters.regex("^سڕینەوە [0-9]+$") & filters.group)
+async def del_message(c, msg):
+    textt = msg.text
+    num = int(textt.split(" ")[1])
+    list1 = []
+    msg_id = msg.id
+    for i in range(1, num):
+        list1.append(msg_id)
+        msg_id = msg_id - 1
+    try:
+        await c.delete_messages(msg.chat.id, list1)
+    except Exception as e:
+        await msg.reply(e)
