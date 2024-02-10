260859c61218d6cad2081eb90239e0bfcbfc27a7
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-10 09:10:11 +0300

    Update playlist.py

--- AlinaXIQ/plugins/play/playlist.py
+++ AlinaXIQ/plugins/play/playlist.py
@@ -57,7 +57,7 @@ from urllib.parse import urlparse
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
-from VIPMUSIC import app
+from AlinaXIQ import app
 from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import Message
