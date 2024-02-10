cc929056394b0fe3c098bc86cdd22e6644e601da
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-10 14:11:39 +0300

    Update ping.py

--- AlinaXIQ/plugins/tools/ping.py
+++ AlinaXIQ/plugins/tools/ping.py
@@ -1,34 +1,28 @@
 from datetime import datetime
+
 from pyrogram import filters
 from pyrogram.types import Message
+
 from AlinaXIQ import app
 from AlinaXIQ.core.call import Alina
 from AlinaXIQ.utils import bot_sys_stats
 from AlinaXIQ.utils.decorators.language import language
 from AlinaXIQ.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
-import aiohttp
-from io import BytesIO
-from pyrogram import filters
 
 
-
-async def make_carbon(code):
-    url = "https://carbonara.solopov.dev/api/cook"
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json={"code": code}) as resp:
-            image = BytesIO(await resp.read())
-    image.name = "carbon.png"
-    return image
-
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping","alive","پشکنین"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    pytgping = await DAXX.ping()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Alina.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    text =  _["ping_2"].format(resp, app.name, UP, RAM, CPU, DISK, pytgping)
-    carbon = await make_carbon(text)
-    await message.reply_photo(carbon)
-    await text.delete()
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
