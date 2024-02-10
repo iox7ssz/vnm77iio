129943818b0aafb6934c1aabbeec58a6a617262f
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-10 08:57:53 +0300

    Update play.py

--- AlinaXIQ/utils/inline/play.py
+++ AlinaXIQ/utils/inline/play.py
@@ -279,8 +279,8 @@ def panel_markup_1(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text= "✚ ᴀᴅᴅ ᴘʟᴀʏʟɪsᴛ ✚",
+                callback_data=f"add_playlist {videoid}",
             ),
         ],
         [
