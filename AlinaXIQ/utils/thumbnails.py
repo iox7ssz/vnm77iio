aa2a4101e906de0a7e8778eb000d6edeec8fcb65
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-10 14:49:38 +0300

    Update thumbnails.py

--- AlinaXIQ/utils/thumbnails.py
+++ AlinaXIQ/utils/thumbnails.py
@@ -107,9 +107,9 @@ async def get_thumb(videoid):
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
     draw = ImageDraw.Draw(background)
-    arial = ImageFont.truetype("AlinaXIQ/assets/assets/font2.ttf", 30)
-    font = ImageFont.truetype("AlinaXIQ/assets/assets/font.ttf", 30)
-    title_font = ImageFont.truetype("AlinaXIQ/assets/assets/font3.ttf", 45)
+    arial = ImageFont.truetype("AlinaXIQ/assets/assets/CreatoDisplay-Medium.ttf", 30)
+    font = ImageFont.truetype("AlinaXIQ/assets/assets/CreatoDisplay-Medium.ttf", 30)
+    title_font = ImageFont.truetype("AlinaXIQ/assets/assets/CreatoDisplay-Medium.ttf", 45)
 
 
     circle_thumbnail = crop_center_circle(youtube, 400, 20)
