0b64b11452fd2cbed2c2b9ad38490f2c6b7c67c1
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-23 20:55:57 +0300

    Update imposter.py

--- AlinaXIQ/plugins/tools/imposter.py
+++ AlinaXIQ/plugins/tools/imposter.py
@@ -1,9 +1,41 @@
+import random 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from AlinaXIQ.plugins.tools.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
 from AlinaXIQ import app
 
+MISHI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
 
+
+ROY = [
+    [
+        InlineKeyboardButton(
+            text="𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿",
+            url=f"https://t.me/IQ7amo")
+    ],
+    [
+        InlineKeyboardButton(text="نوێکارییەکانی ئەلینا 🍻", url=f"https://t.me/MGIMT")
+    ],
+]
 
 
 @app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=69)
@@ -25,11 +57,10 @@ async def chk_usr(_, message: Message):
         or lastname_before != message.from_user.last_name
     ):
         msg += f"""
-**🔓 ᴘʀᴇᴛᴇɴᴅᴇʀ ᴅᴇᴛᴇᴄᴛᴇᴅ 🔓**
-➖➖➖➖➖➖➖➖➖➖➖➖
-**🍊 ɴᴀᴍᴇ** : {message.from_user.mention}
-**🍅 ᴜsᴇʀ ɪᴅ** : {message.from_user.id}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**♥︎ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ♥︎**
+
+**๏ ɴᴀᴍᴇ** ➛ {message.from_user.mention}
+**๏ ᴜsᴇʀ ɪᴅ** ➛ {message.from_user.id}
 """
     if usernamebefore != message.from_user.username:
         usernamebefore = f"@{usernamebefore}" if usernamebefore else "NO USERNAME"
@@ -39,11 +70,10 @@ async def chk_usr(_, message: Message):
             else "NO USERNAME"
         )
         msg += """
-**🐻‍❄️ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ 🐻‍❄️**
-➖➖➖➖➖➖➖➖➖➖➖➖
-**🎭 ғʀᴏᴍ** : {bef}
-**🍜 ᴛᴏ** : {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**♥︎ ᴄʜᴀɴɢᴇᴅ ᴜsᴇʀɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(bef=usernamebefore, aft=usernameafter)
         await add_userdata(
             message.from_user.id,
@@ -53,11 +83,10 @@ async def chk_usr(_, message: Message):
         )
     if first_name != message.from_user.first_name:
         msg += """
-**🪧 ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ 🪧**
-➖➖➖➖➖➖➖➖➖➖➖➖
-**🔐 ғʀᴏᴍ** : {bef}
-**🍓 ᴛᴏ** : {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**♥︎ ᴄʜᴀɴɢᴇs ғɪʀsᴛ ɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(
             bef=first_name, aft=message.from_user.first_name
         )
@@ -71,11 +100,10 @@ async def chk_usr(_, message: Message):
         lastname_before = lastname_before or "NO LAST NAME"
         lastname_after = message.from_user.last_name or "NO LAST NAME"
         msg += """
-**🪧 ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ 🪧**
-➖➖➖➖➖➖➖➖➖➖➖➖
-**🚏ғʀᴏᴍ** : {bef}
-**🍕 ᴛᴏ** : {aft}
-➖➖➖➖➖➖➖➖➖➖➖➖\n
+**♥︎ ᴄʜᴀɴɢᴇs ʟᴀsᴛ ɴᴀᴍᴇ ♥︎**
+
+**๏ ʙᴇғᴏʀᴇ** ➛ {bef}
+**๏ ᴀғᴛᴇʀ** ➛ {aft}
 """.format(
             bef=lastname_before, aft=lastname_after
         )
@@ -86,7 +114,7 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
     if msg != "":
-        await message.reply_photo("https://telegra.ph/file/6b0a0f76bf5660454ae89.jpg", caption=msg)
+        await message.reply_photo(random.choice(MISHI), caption=msg, reply_markup=InlineKeyboardMarkup(ROY),)
 
 
 @app.on_message(filters.group & filters.command("imposter") & ~filters.bot & ~filters.via_bot)
@@ -109,3 +137,5 @@ async def set_mataa(_, message: Message):
             await message.reply(f"**sᴜᴄᴄᴇssғᴜʟʟʏ ᴅɪsᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴍᴏᴅᴇ ғᴏʀ** {message.chat.title}")
     else:
         await message.reply("**ᴅᴇᴛᴇᴄᴛ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴇʀs ᴜsᴀɢᴇ : ᴘʀᴇᴛᴇɴᴅᴇʀ ᴏɴ|ᴏғғ**")
+
+    
