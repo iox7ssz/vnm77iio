ac8d0f9920e8232ca02d00880688e09e4fe630af
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-24 21:00:52 +0300

    Update welcome.py

--- AlinaXIQ/plugins/tools/welcome.py
+++ AlinaXIQ/plugins/tools/welcome.py
@@ -132,7 +132,7 @@ async def auto_state(_, message):
 
 
 @app.on_chat_member_updated(filters.group, group=-3)
-async def greet_new_member(_, member: ChatMemberUpdated):
+async def greet_new_member(_, message, member: ChatMemberUpdated):
     chat_id = member.chat.id
     count = await app.get_chat_members_count(chat_id)
     A = await wlcm.find_one(chat_id)
