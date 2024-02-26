0c9de62ac752b9dae587d414ae1781d8b5474eb0
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-26 19:02:24 +0300

    Update Alinaban.py

--- AlinaXIQ/plugins/sudo/Alinaban.py
+++ AlinaXIQ/plugins/sudo/Alinaban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["qm","qmc"], prefixes=["I", "i"]) & admin_filter)
+@app.on_message(filters.command(["lina","q"], prefixes=["A", "a","i"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
