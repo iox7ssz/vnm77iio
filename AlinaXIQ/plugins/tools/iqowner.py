90e647d07fbffd0e0b50afcf372317f4bb68fd92
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-29 19:34:34 +0300

    Update iqowner.py

--- AlinaXIQ/plugins/tools/iqowner.py
+++ AlinaXIQ/plugins/tools/iqowner.py
@@ -10,6 +10,7 @@ from AlinaXIQ.utils.database import add_served_chat, delete_served_chat
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from AlinaXIQ.utils.database import get_assistant
 import asyncio
+from strings.filters import command
 from AlinaXIQ.misc import SUDOERS
 from AlinaXIQ.core.userbot import Userbot
 from pyrogram import Client, filters
@@ -41,36 +42,42 @@ async def bot_check(_, message):
 
 # --------------------------------------------------------------------------------- #
 
-
+import time
 
 
 @app.on_message(command(["/addbots", "زیادکردنی بۆت", "/addbot", f"/addbots@{app.username}"]) & SUDOERS)
 async def add_all(client, message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 2:
+        await message.reply("**🧑🏻‍💻┋ فەرمانت هەڵە بەکار‌هێنا بەم شێوازە بنووسە :\n/addbots @bot_username**")
+        return
     
-    done = 0
-    failed = 0
-    lol = await message.reply("**✅┋ زیادکردنی بۆت لە هەموو گرووپەکان**")
+    bot_username = command_parts[1]
     try:
         userbot = await get_assistant(message.chat.id)
+        app_id = (await app.get_users(bot_username)).id
+        done = 0
+        failed = 0
+        lol = await message.reply("**✅┋ زیادکردنی بۆت لە هەموو گرووپەکان**")
+        
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == -1001962701094:
                 continue
             try:
-                await userbot.add_chat_members(dialog.chat.id, app.id)
+                await userbot.add_chat_members(dialog.chat.id, app_id)
                 done += 1
                 await lol.edit(
-                    message.chat.id,
-                    f"**✅┋ یاریدەدەر بۆتی زیادکرد بۆ {done} گرووپ**"
+                    f"**✅┋ یاریدەدەر {bot_username} زیادی کرد\n🧑🏻‍💻┋ بۆ {done} گرووپ\n(Timestamp: {time.time()})**"
                 )
             except Exception as e:
                 failed += 1
                 await lol.edit(
-                    message.chat.id,
-                    f"**❌┋ شکستی هێنا لە زیادکردنی بۆت**"
+                    f"**❌┋ شکستی هێنا لە زیادکردنی {bot_username} بۆ گرووپ\n(Timestamp: {time.time()})**"
                 )
-            await asyncio.sleep(1)  # Adjust sleep time based on rate limits
-    finally:
+            await asyncio.sleep(2)  # Adjust sleep time based on rate limits
+        
         await lol.edit(
-            message.chat.id,
-            f"**✅┋ زیادکرا بۆ : {done} گرووپ\n❌┋ شکستی هێنا لە : {failed} گرووپ**"
+            f"**✅┋ زیادکرا {bot_username}\n🧑🏻‍💻┋ بۆ {done} گرووپ\n❌┋ شکستی هێنا لە {failed} گرووپ\n(Timestamp: {time.time()})**"
         )
+    except Exception as e:
+        await message.reply(f"**❌┋ هەڵە : {str(e)}**")
