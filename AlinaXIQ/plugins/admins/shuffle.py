bcaa4607a3c6689a0f3df1f5f2e76c446eff9f8a
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-15 02:07:39 +0300

    Update shuffle.py

--- AlinaXIQ/plugins/admins/shuffle.py
+++ AlinaXIQ/plugins/admins/shuffle.py
@@ -30,5 +30,5 @@ async def admins(Client, message: Message, _, chat_id):
     random.shuffle(check)
     check.insert(0, popped)
     await message.reply_text(
-        _["admin_16"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_16"].format(user_mention), reply_markup=close_markup(_)
     )
