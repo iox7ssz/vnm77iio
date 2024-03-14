14786549507c0e224ca400921172dcdbbeb3f3e8
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-14 23:45:26 +0300

    Update resume.py

--- AlinaXIQ/plugins/admins/resume.py
+++ AlinaXIQ/plugins/admins/resume.py
@@ -15,6 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
     await Alina.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
