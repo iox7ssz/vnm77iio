f29b378d4dacf4d459ed8d2eb51451dfa37f02d0
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-25 23:50:09 +0300

    Update call.py

--- AlinaXIQ/core/call.py
+++ AlinaXIQ/core/call.py
@@ -6,10 +6,10 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
+from ntgcalls import TelegramServerError
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
-    TelegramServerError,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
